f441260ee1eef908636068b3d1a4af25c01db6e5
Pierre <pierre.jambet@gmail.com>
2022-05-07 19:31:04 -0400

    Tests work!
    Lesson learned, don't redirect when calling spawn

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 
 source "https://rubygems.org"
 
+gem "debug"
 gem "minitest"
 gem "minitest-focus"
 gem "minitest-reporters"
--- test/server_test.rb
+++ test/server_test.rb
@@ -5,74 +5,73 @@ require 'timeout'
 
 describe "A server" do
   it "can be connected to" do
-    with_server do |s|
-      assert s
+    with_server do
+      connect_to_server do |s|
+        assert s
+      end
     end
   end
 
   it "responds to GET" do
-    with_server do |s|
-      s.puts("GET a")
-      assert_equal "\n", s.gets
+    with_server do
+      connect_to_server do |s|
+        s.puts("GET a")
+        assert_equal "\n", s.gets
 
-      s.puts("GET b")
-      assert_equal "\n", s.gets
+        s.puts("GET b")
+        assert_equal "\n", s.gets
+      end
     end
   end
 
   it "responds to SET" do
-    with_server do |s|
-      s.puts("SET a b")
-      assert_equal "OK\n", s.gets
+    with_server do
+      connect_to_server do |s|
+        s.puts("SET a b")
+        assert_equal "OK\n", s.gets
 
-      s.puts("GET a")
-      assert_equal "b\n", s.gets
+        s.puts("GET a")
+        assert_equal "b\n", s.gets
+      end
     end
   end
 
   it "respond to QUIT" do
-    with_server do |s|
-      s.puts("QUIT")
-      assert s.eof?
+    with_server do
+      connect_to_server do |s|
+        s.puts("QUIT")
+        assert s.eof?
+      end
     end
   end
 
   it "handles multiple clients" do
-    server_pid = spawn("./go/server 7878 > /dev/null 2>&1")
-    socket = nil
-    # puts socket.nil?
-    # puts !socket&.nil?
+    with_server do
+      socket = nil
 
-    loop do
-      begin
-        socket = TCPSocket.new('localhost', 7878)
-        # puts "CONNECTED!"
-        break
-      rescue Errno::ECONNREFUSED => _e
-        sleep 0.001
-      end
-    end
-
-    # Process.detach(server_pid)
-
-    pids = 3.times.map do
-      run_in_process do
-        socket = TCPSocket.new('localhost', 7878)
-        # puts "Connected"
-        # puts "I'm in a different process"
-        # puts Process.pid
-        2.times do
-          socket.puts "SET #{ SecureRandom.uuid } #{ rand(10) }"
-          sleep 0.01
-          socket.puts "GET #{ SecureRandom.uuid }"
+      loop do
+        begin
+          socket = TCPSocket.new('localhost', 7878)
+          break
+        rescue Errno::ECONNREFUSED => _e
+          sleep 0.001
         end
       end
-    end
 
-    # puts "waiting:"
-    pids.each { Process.wait(_1) }
-    Process.kill 'KILL', server_pid
-    assert true
+      pids = 3.times.map do
+        run_in_process do
+          socket = TCPSocket.new('localhost', 7878)
+          2.times do
+            socket.puts "SET #{ SecureRandom.uuid } #{ rand(10) }"
+            sleep 0.01
+            socket.puts "GET #{ SecureRandom.uuid }"
+          end
+        end
+      end
+
+      pids.each { |pid| Process.wait(pid) }
+      assert true
+    end
   end
 
   def run_in_process
@@ -81,12 +80,8 @@ describe "A server" do
     end
   end
 
-  def with_server
-    system("go build -o ./go/server go/server.go") # unless File.exist?("./go/server")
-    # puts Dir.getwd
-    # puts system("whoami")
-    pid = spawn("./go/server 7878 > /dev/null 2>&1")
-
+  def connect_to_server
+    socket = nil
     Timeout.timeout(5) do
       loop do
         begin
@@ -94,11 +89,19 @@ describe "A server" do
           yield socket
           break
         rescue StandardError => e
-          # puts e.message
           sleep 0.1
         end
       end
     end
+  ensure
+    socket.close if socket
+  end
+
+  def with_server
+    system("go build -o ./go/server go/server.go") # unless File.exist?("./go/server")
+    pid = spawn("./go/server 7878", STDOUT => "/dev/null")
+
+    yield
 
   ensure
     if pid
