a71c23665cdcceef632a268591f1046d173f4f7f
Pierre Jambet <pjam@fastmail.com>
2024-01-05 01:08:07 -0500

    python: passes tests
    saw some random failures but seems very rare, main issue was that we
    were not catching errors when writing back

--- python/server.py
+++ python/server.py
@@ -1,6 +1,5 @@
 import select
 import socket
-import sys
 
 sockets = []
 db = {}
@@ -9,46 +8,85 @@ def handle_client(client):
     sockets.append(client)
 
 def handle_request(client):
-    print("Request from client")
-    data = client.recv(1024).decode("utf-8").strip()
+    try:
+      data = client.recv(1024).decode("utf-8").strip()
+    except ConnectionResetError:
+        client.close()
+        del sockets[sockets.index(client)]
+        return
+
     parts = data.split(" ")
-    print("data", data)
-    key = parts[1]
+    try:
+      key = parts[1]
+    except IndexError:
+      key = None
     response = None
-    print("parts: ", parts)
-    print("db: ", db)
-    
+
     if data.startswith("GET") and key:
         if key in db:
-            response = db[key] + "\n"
+            response = db[key]
         else:
-            response = "\n"
-
+            response = ""
     elif data.startswith("SET") and key:
-        value = parts[2]
-        print("Setting: ", key, value)
+        try:
+          value = parts[2]
+        except IndexError:
+          value = None
+
         if value:
             db[key] = value
-            response = "OK\n"
+            response = "OK"
         else:
-            response = "\n"
+            response = "N/A"
+    elif data.startswith("DEL"):
+        if key:
+            if key in db:
+                del db[key]
+                response = "1"
+            else:
+                response = "0"
+        else:
+            response = "N/A"
+    elif data.startswith("INCR"):
+        if key:
+            if key in db:
+                try:
+                    existing_value = int(db[key])
+                    new_value = str(existing_value + 1)
+                    db[key] = new_value
+                    response = new_value
+                except ValueError:
+                    response = "ERR value is not an integer or out of range"
+            else:
+                db[key] = "1"
+                response = "1"
+        else:
+          response = "N/A"
     elif data.startswith("QUIT"):
         client.close()
+        del sockets[sockets.index(client)]
         return
     else:
-        response = "N/A\n"
+        response = "N/A"
 
-    client.send(str.encode(response))
+    try:
+      client.send(str.encode(response + "\n"))
+    except BrokenPipeError:
+        client.close()
+        del sockets[sockets.index(client)]
 
 if __name__ == '__main__':
+    server_address = ('localhost', 3000)
+    print('starting up on', server_address)
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setblocking(0)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_address = ('localhost', 3000)
-    print('starting up on', server_address)
     server.bind(server_address)
     server.listen(5)
+
     sockets.append(server)
+
     while True:
         readable, writable, exceptional = select.select(sockets, [], [])
         for socket in readable:
