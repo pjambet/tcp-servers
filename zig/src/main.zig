11ae79d52c0744a154fcc1f0e344293083aeaa18
Pierre Jambet <pjam@fastmail.com>
2024-01-06 16:18:59 -0500

    zig: server runs-ish

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const net = std.net;
 const fs = std.fs;
 const os = std.os;
+const ArrayList = std.ArrayList;
 
 pub const io_mode = .evented;
 
@@ -9,81 +10,67 @@ pub fn main() anyerror!void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = general_purpose_allocator.allocator();
 
-    var server = net.StreamServer.init(.{});
+    var server = net.StreamServer.init(.{
+        .reuse_address = true,
+        .reuse_port = true,
+    });
     defer server.deinit();
 
-    // TODO handle concurrent accesses to this hash map
-    var room = Room{ .clients = std.AutoHashMap(*Client, void).init(allocator) };
-
-    try server.listen(net.Address.parseIp("127.0.0.1", 0) catch unreachable);
+    try server.listen(net.Address.parseIp("127.0.0.1", 3000) catch unreachable);
     std.debug.print("listening at {}\n", .{server.listen_address});
     std.debug.print("Server.sockfd {?}\n", .{server.sockfd});
 
-    // var fd_set = os.fd_set{};
-    // std.debug.print("fd_set {}\n", .{fd_set});
+    const server_fd = server.sockfd.?;
 
-    const fd = server.sockfd.?;
+    var pollfds = ArrayList(os.pollfd).init(allocator);
+    defer pollfds.deinit();
 
-    var fds2: [1]os.pollfd = .{.{ .fd = fd, .events = os.POLL.IN, .revents = 0 }};
+    var clients = std.AutoArrayHashMap(i32, net.StreamServer.Connection).init(allocator);
+    defer clients.deinit();
 
     while (true) {
-        const client = try allocator.create(Client);
-        std.debug.print("Polling\n", .{});
-        _ = try os.poll(&fds2, 15000);
-        std.debug.print("Done polling\n", .{});
+        pollfds.clearRetainingCapacity();
+        // Add the server first
+        try pollfds.append(.{ .fd = server_fd, .events = os.POLL.IN, .revents = 0 });
+        // Then all connected clients
+        for (clients.values()) |client| {
+            try pollfds.append(.{ .fd = client.stream.handle, .events = os.POLL.IN, .revents = 0 });
+        }
 
-        client.* = Client{
-            .conn = try server.accept(),
-            .handle_frame = async client.handle(&room),
-        };
-        try room.clients.putNoClobber(client, {});
+        var poll_result = try os.poll(pollfds.items, 15000);
+        std.debug.print("Poll result {?}\n", .{poll_result});
+        for (pollfds.items) |pollfd| {
+            std.debug.print("items after poll {?}\n", .{pollfd});
+        }
+        // std.debug.print("items after poll {?}\n", .{pollfds.values[0]});
+        for (pollfds.items) |pollfd| {
+            if (pollfd.revents & os.POLL.HUP != 0) {
+                std.debug.print("HUP, removing and continuing\n", .{});
+                _ = clients.orderedRemove(pollfd.fd);
+                break;
+            } else if (pollfd.revents & os.POLL.IN == 1) {
+                if (pollfd.fd == server_fd) {
+                    std.debug.print("Done polling\n", .{});
+                    var conn = try server.accept();
+                    // defer conn.stream.close();
+                    std.debug.print("conn handle {?}\n", .{conn.stream.handle});
+                    try clients.put(conn.stream.handle, conn);
+                    _ = try conn.stream.write("server: welcome to the chat server\n");
+                } else {
+                    var conn = clients.get(pollfd.fd).?;
+                    var buf: [100]u8 = undefined;
+                    const amt = try conn.stream.read(&buf);
+                    const msg = buf[0..amt];
+                    std.debug.print("received: {?}\n", .{amt});
+                    std.debug.print("received: {s}\n", .{msg});
+                    _ = try conn.stream.write("server: You said something, thanks\n");
+                }
+            }
+        }
     }
+
+    std.os.exit(0);
 }
-const Client = struct {
-    conn: net.StreamServer.Connection,
-    handle_frame: @Frame(handle),
-
-    fn handle(self: *Client, room: *Room) !void {
-        _ = try self.conn.stream.write("server: welcome to teh chat server\n");
-        while (true) {
-            var buf: [100]u8 = undefined;
-            const amt = try self.conn.stream.read(&buf);
-            const msg = buf[0..amt];
-            room.broadcast(msg, self);
-        }
-    }
-};
-const Room = struct {
-    clients: std.AutoHashMap(*Client, void),
-
-    fn broadcast(room: *Room, msg: []const u8, sender: *Client) void {
-        var it = room.clients.keyIterator();
-        while (it.next()) |key_ptr| {
-            const client = key_ptr.*;
-            if (client == sender) continue;
-            _ = client.conn.stream.write(msg) catch |e| std.debug.print("unable to send: {}\n", .{e});
-        }
-    }
-};
-
-// const std = @import("std");
-// const os = std.os;
-
-// pub fn main() !void {
-//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-//     // stdout is for the actual output of your application, for example if you
-//     // are implementing gzip, then only the compressed bytes should be sent to
-//     // stdout, not any debugging messages.
-//     const stdout_file = std.io.getStdOut().writer();
-//     var bw = std.io.bufferedWriter(stdout_file);
-//     const stdout = bw.writer();
-
-//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-//     try bw.flush(); // don't forget to flush!
-// }
 
 // test "simple test" {
 //     var list = std.ArrayList(i32).init(std.testing.allocator);
