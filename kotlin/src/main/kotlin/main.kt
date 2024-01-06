0276f25390d03d2966afb6f987d96ab1c253145f
Pierre Jambet <pjam@fastmail.com>
2024-01-05 23:01:38 -0500

    kotlin: passes test

--- kotlin/src/main/kotlin/main.kt
+++ kotlin/src/main/kotlin/main.kt
@@ -12,10 +12,13 @@ import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
 
+abstract sealed class Operation(key: String, channel: Channel<String?>)
+data class GetOperation(val key: String, val channel: Channel<String?>) : Operation(key, channel)
+data class SetOperation(val key: String, val value: String, val channel: Channel<String?>) : Operation(key, channel)
+data class DelOperation(val key: String, val channel: Channel<String?>) : Operation(key, channel)
+data class IncrOperation(val key: String, val channel: Channel<String?>) : Operation(key, channel)
+
 suspend fun runServer(opChannel: Channel<Operation>) = coroutineScope {
-    val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
-        log.error("Handle $exception in CoroutineExceptionHandler")
-    }
     launch(Dispatchers.IO) {
         val server = ServerSocket(3000)
         log.info("Listening on port 3000")
@@ -23,7 +26,7 @@ suspend fun runServer(opChannel: Channel<Operation>) = coroutineScope {
         while(true) {
             val client = server.accept()
             log.info("Starting handler for: $client")
-            launch {
+            launch(Dispatchers.IO) {
                 try {
                     handleClient(client, opChannel)
                 } catch (t: Throwable) {
@@ -38,46 +41,69 @@ suspend fun runServer(opChannel: Channel<Operation>) = coroutineScope {
 
 suspend fun handleClient(client: Socket, channel: Channel<Operation>) {
     log.info("Handling client: $client")
-    while (true) {
-        val output = PrintWriter(client.getOutputStream(), true)
-        val input = BufferedReader(InputStreamReader(client.inputStream)).readLine() ?: return
-        println("input: $input")
+    withContext(Dispatchers.IO) {
+        while (true) {
+            val output = PrintWriter(client.getOutputStream(), true)
+            val input = BufferedReader(InputStreamReader(client.inputStream)).readLine()
+            if (input == null) {
+                client.close()
+                return@withContext
+            }
 
-        if (input.startsWith("STOP") || input.startsWith("QUIT")) {
-            client.close()
-            return
-        } else if (input.startsWith("GET")) {
-            var key: String?
-            try {
-                key = input.split(" ").getOrNull(1)
-            } catch(e: Exception) {
-                throw e
+            if (input.startsWith("STOP") || input.startsWith("QUIT")) {
+                client.close()
+                return@withContext
+            } else if (input.startsWith("GET")) {
+                var key: String?
+                try {
+                    key = input.split(" ").getOrNull(1)
+                } catch (e: Exception) {
+                    throw e
+                }
+                if (key != null) {
+                    val chan = Channel<String?>()
+                    channel.send(GetOperation(key, chan))
+                    val result = chan.receive() ?: ""
+                    output.println(result)
+                } else {
+                    output.println("")
+                }
+            } else if (input.startsWith("SET")) {
+                val key = input.split(" ").getOrNull(1)
+                val value = input.split(" ").getOrNull(2)
+                if (key != null && value != null) {
+                    val chan = Channel<String?>(RENDEZVOUS)
+                    channel.send(SetOperation(key, value, chan))
+                    output.println("OK")
+                } else {
+                    output.println("")
+                }
+            } else if (input.startsWith("DEL")) {
+                val key = input.split(" ").getOrNull(1)
+                if (key != null) {
+                    val chan = Channel<String?>(RENDEZVOUS)
+                    channel.send(DelOperation(key, chan))
+                    val result = chan.receive() ?: ""
+                    output.println(result)
+                } else {
+                    output.println("")
+                }
+            } else if (input.startsWith("INCR")) {
+                val key = input.split(" ").getOrNull(1)
+                if (key != null) {
+                    val chan = Channel<String?>(RENDEZVOUS)
+                    channel.send(IncrOperation(key, chan))
+                    val result = chan.receive() ?: ""
+                    output.println(result)
+                } else {
+                    output.println("")
+                }
+            } else if (input.startsWith("RAISE")) {
+                throw Throwable("foo de fafa")
             }
-            if (key != null) {
-                val chan = Channel<String?>()
-                channel.send(Operation(key, null, chan))
-                val result = chan.receive() ?: ""
-                output.println(result)
-            } else {
-                output.println("")
-            }
-        } else if (input.startsWith("SET")) {
-            val key = input.split(" ").getOrNull(1)
-            val value = input.split(" ").getOrNull(2)
-            if (key != null && value != null) {
-                val chan = Channel<String?>(RENDEZVOUS)
-                channel.send(Operation(key, value, chan))
-                output.println("OK")
-            } else {
-                output.println("")
-            }
-        } else if (input.startsWith("RAISE")) {
-            throw Throwable("foo de fafa")
         }
     }
 }
-
-data class Operation(val key: String, val value: String?, val chan: Channel<String?>)
 
 val log: Logger = LoggerFactory.getLogger("Coroutines")
 
@@ -86,17 +112,41 @@ fun main() {
     val state = mutableMapOf<String, String>()
 
     GlobalScope.launch {
-        log.info("Starting op loop")
         while(true) {
             val op = opChannel.receive()
-            if (op.value == null) {
-                log.debug("GET request")
-                op.chan.send(state[op.key])
-                op.chan.close()
-            } else {
-                log.debug("SET request")
-                state[op.key] = op.value
-                op.chan.close()
+            when (op) {
+                is GetOperation -> {
+                    op.channel.send(state[op.key])
+                    op.channel.close()
+                }
+                is SetOperation -> {
+                    state[op.key] = op.value
+                    op.channel.close()
+                }
+                is DelOperation -> {
+                    val removed = state.remove(op.key)
+                    val response = if (removed == null) "0" else "1"
+                    op.channel.send(response)
+                    op.channel.close()
+                }
+                is IncrOperation -> {
+                    val existingValue = state[op.key]
+                    val response = if (existingValue != null) {
+                        val existingInt = existingValue.toIntOrNull()
+                        if (existingInt == null) {
+                            "ERR value is not an integer or out of range"
+                        } else {
+                            val newValue = (existingInt + 1).toString()
+                            state[op.key] = newValue
+                            newValue
+                        }
+                    } else {
+                        state[op.key] = "1"
+                        "1"
+                    }
+                    op.channel.send(response)
+                    op.channel.close()
+                }
             }
         }
     }
