6f040a83c8f9d4e390538db1f6f3ffbc7f8579fa
Pierre Jambet <pjam@fastmail.com>
2024-01-06 23:38:40 -0500

    scala2: it's a start

--- scala2/tcp-server/build.sbt
+++ scala2/tcp-server/build.sbt
@@ -1,6 +1,6 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.10"
+ThisBuild / scalaVersion := "2.13.12"
 
 lazy val root = (project in file("."))
   .settings(
--- scala2/tcp-server/src/main/scala/Main.scala
+++ scala2/tcp-server/src/main/scala/Main.scala
@@ -1,14 +1,59 @@
+import java.net.{ServerSocket, Socket}
+import java.io.{BufferedReader, InputStreamReader, PrintWriter}
+import java.util.concurrent.{ConcurrentHashMap, Executors}
+import scala.collection.mutable
 import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.Duration
 
 object Main {
+
+  private def accept(
+      server: ServerSocket
+  )(implicit ec: ExecutionContext): Future[Socket] = {
+    Future { server.accept() }
+  }
+
+  private def handleClient(
+      client: Socket,
+      db: ConcurrentHashMap[String, String],
+      executionContext: ExecutionContext
+  ): Future[Unit] = {
+    Future {
+      while (true) {
+        val input = new BufferedReader(new InputStreamReader(client.getInputStream)).readLine()
+        if (input == null) {
+          client.close()
+          return Future.successful(())
+        }
+
+        val output = new PrintWriter(client.getOutputStream, true)
+        output.println(s"Hello: $input 👋")
+      }
+    }(executionContext)
+  }
+
+  private def continuouslyAccept(
+      server: ServerSocket,
+      clients: collection.mutable.Set[Socket],
+      db: ConcurrentHashMap[String, String],
+      executionContext: ExecutionContext
+  )(implicit ec: ExecutionContext): Future[Unit] = {
+    accept(server).flatMap { client =>
+      println(s"accepted $client")
+      clients.add(client)
+      val _ = handleClient(client, db, executionContext)
+      continuouslyAccept(server, clients, db, executionContext)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     import ExecutionContext.Implicits.global
-
-    val f = Future {
-      "Hello world!"
-    }
-
-    Await.result(f.map { println(_) }, 1.second)
+//collection.immutable.M
+    val db = new ConcurrentHashMap[String, String]()
+    val executor = Executors.newVirtualThreadPerTaskExecutor()
+    val ec = ExecutionContext.fromExecutor(executor)
+    val clients = collection.mutable.Set[Socket]()
+    val server = new ServerSocket(3000)
+    Await.result(continuouslyAccept(server, clients, db, ec), Duration.Inf)
   }
 }
