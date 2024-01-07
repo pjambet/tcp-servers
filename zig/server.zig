063c74206d7e8242e914ff1b5b577bcfa534c0ba
Pierre Jambet <pjam@fastmail.com>
2024-01-07 17:20:58 -0500

    zig: took a while but I think it's decent now
    - We use allocPrint to create a formatted starting
    - We use BufMap, which copies and owns the keys and values, still not
      sure if that other hashmap could have fit my use case, who knows.

--- zig/server.zig
+++ zig/server.zig
@@ -1,10 +1,106 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const os = std.os;
 
-pub fn main() !void {
-    var fds2: [1]os.pollfd = .{.{ .fd = os.STDOUT_FILENO, .events = os.POLL.OUT, .revents = 0 }};
-    _ = try os.poll(&fds2, 1);
+const CustomEntry = struct {
+    string: []const u8 = undefined,
+    key_p: []u8 = undefined,
+};
 
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {s}!\n", .{"world"});
+pub fn main() !void {
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = general_purpose_allocator.allocator();
+
+    defer {
+        const deinit_status = general_purpose_allocator.deinit();
+        //fail test; can't try in defer as defer is executed after we return
+        if (deinit_status == .leak) expect(false) catch @panic("TEST FAIL");
+    }
+
+    // var fd: [1]os.pollfd = .{.{ .fd = os.STDOUT_FILENO, .events = os.POLL.OUT, .revents = 0 }};
+    // _ = try os.poll(&fds2, 1);
+
+    // const stdout = std.io.getStdOut().writer();
+    // try stdout.print("Hello, {s}!\n", .{"world"});
+
+    // var hashMap = std.StringHashMap([]const u8).init(allocator);
+    // var hashMap = std.StringHashMap(*CustomEntry).init(allocator);
+    var hashMap = std.BufMap.init(allocator);
+    defer hashMap.deinit();
+    {
+        // try hashMap.put("foo", &"bar");
+        // var key: []const u8 = "foo";
+        // var bar: []const u8 = "bar";
+        // var keySlice = try allocator.dupe(u8, key);
+        // var barSlice = try allocator.dupe(u8, bar);
+        // defer allocator.free(keySlice);
+        // defer allocator.free(barSlice);
+        // allocator.free(barSlice);
+        // std.debug.print("keySlice: {}\n", .{&keySlice});
+        // std.debug.print("barSlice: {}\n", .{&barSlice});
+        // try hashMap.put(keySlice, barSlice);
+        var key = try std.fmt.allocPrint(allocator, "{s}", .{"foo"});
+        defer allocator.free(key);
+        var value = try std.fmt.allocPrint(allocator, "{s}", .{"123"});
+        defer allocator.free(value);
+
+        std.debug.print("key: {s}\n", .{key});
+        std.debug.print("key: {d}\n", .{key});
+        std.debug.print("key: {}\n", .{&key});
+
+        std.debug.print("value: {s}\n", .{value});
+        std.debug.print("value: {}\n", .{&value});
+        // var node = try allocator.create(Node);
+        // errdefer allocator.destroy(node);
+        // node.* = .{ .next = self.head, .item = item, .prev = null };
+        // var entry_v = try allocator.create(CustomEntry);
+        // defer allocator.destroy(entry_v);
+        // entry_v.* = CustomEntry{
+        //     .string = value,
+        //     .key_p = key,
+        // };
+
+        try hashMap.put(key, value);
+        // _ = hashMap.remove("foo");
+        // // std.debug.print("foo: {s}\n", .{hashMap.get("foo").?});
+        // std.debug.print("foo: {s}\n", .{hashMap.get("foo").?});
+        // var entryOpt = hashMap.getEntry("foo");
+        // if (entryOpt) |entry| {
+        //     std.debug.print("entry: {}\n", .{entry});
+        //     std.debug.print("entry.key_ptr: {}\n", .{entry.key_ptr});
+        //     std.debug.print("entry.value_ptr: {}\n", .{entry.value_ptr});
+        //     std.debug.print("entry.key_ptr.*: {s}\n", .{entry.key_ptr.*});
+        //     // std.debug.print("entry.value_ptr.*.key_p: {s}\n", .{entry.value_ptr.*.key_p});
+        //     // std.debug.print("entry.value_ptr.*.key_p: {d}\n", .{entry.value_ptr.*.key_p});
+        //     // std.debug.print("entry.value_ptr.*.*: {s}\n", .{entry.value_ptr.*.*});
+        //     // std.debug.print("&entry.value_ptr.*: {s}\n", .{&entry.value_ptr.*});
+
+        //     // allocator.free(entry.key_ptr.*);
+        //     // allocator.free(entry.value_ptr.*);
+        //     _ = hashMap.remove("foo");
+        //     //     // allocator.free(entry.key_ptr.*);
+        //     defer allocator.free(entry.value_ptr.*);
+        // }
+
+        // var entryOpt2 = hashMap.getEntry("foo");
+        var contains = hashMap.get("foo");
+        if (contains) |_| {
+            _ = hashMap.remove("foo");
+            std.debug.print("Found entry\n", .{});
+        } else {
+            std.debug.print("No entry\n", .{});
+        }
+
+        // if (hashMap.get("foo")) |value| {
+        //     std.debug.print("foo: {s}\n", .{value});
+        // } else {
+        //     std.debug.print("foo: NO_VALUE\n", .{});
+        // }
+    }
+    const bytes = try allocator.alloc(u8, 100);
+    defer allocator.free(bytes);
+    // allocator.free(barSlice);
+    // std.debug.print("foo: {s}\n", .{barSlice});
+
+    // hashMap.clearAndFree();
 }
--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const net = std.net;
 const fs = std.fs;
 const os = std.os;
@@ -9,6 +10,11 @@ pub const io_mode = .evented;
 pub fn main() anyerror!void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = general_purpose_allocator.allocator();
+    defer {
+        const deinit_status = general_purpose_allocator.deinit();
+        //fail test; can't try in defer as defer is executed after we return
+        if (deinit_status == .leak) expect(false) catch @panic("TEST FAIL");
+    }
 
     var server = net.StreamServer.init(.{
         .reuse_address = true,
@@ -17,6 +23,7 @@ pub fn main() anyerror!void {
     defer server.deinit();
 
     try server.listen(net.Address.parseIp("127.0.0.1", 3000) catch unreachable);
+    std.debug.print("Yo!\n", .{});
     std.debug.print("listening at {}\n", .{server.listen_address});
     std.debug.print("Server.sockfd {?}\n", .{server.sockfd});
 
@@ -28,13 +35,14 @@ pub fn main() anyerror!void {
     var clients = std.AutoArrayHashMap(i32, net.StreamServer.Connection).init(allocator);
     defer clients.deinit();
 
-    // var db = std.StringHashMap(void).init(allocator);
-    var db = std.StringArrayHashMap([]const u8).init(allocator);
+    // var db = std.StringHashMap([]const u8).init(allocator);
+    var db = std.BufMap.init(allocator);
+    // var db = std.StringArrayHashMap([]const u8).init(allocator);
     defer db.deinit();
-    try db.put("foo", "bar");
+    // try db.put("foo", "bar");
     // try db.put("foo", undefined);
 
-    while (true) {
+    outer: while (true) {
         pollfds.clearRetainingCapacity();
         // Add the server first
         try pollfds.append(.{ .fd = server_fd, .events = os.POLL.IN, .revents = 0 });
@@ -67,50 +75,59 @@ pub fn main() anyerror!void {
                     var buf: [100]u8 = undefined;
                     const amt = try conn.stream.read(&buf);
                     const msg = buf[0..amt];
-                    std.debug.print("received: {?}\n", .{amt});
-                    std.debug.print("received: {s}\n", .{msg});
+                    std.debug.print("received: '{?}'\n", .{amt});
+                    std.debug.print("received: '{s}'\n", .{msg});
+                    std.debug.print("received: '{d}'\n", .{msg});
                     var partsIterator = std.mem.splitScalar(u8, msg, ' ');
                     var parts = ArrayList([]const u8).init(allocator);
-                    // defer parts.deinit();
+                    defer parts.deinit();
                     while (partsIterator.next()) |part| {
-                        try parts.append(std.mem.trim(u8, part, "\n"));
+                        try parts.append(std.mem.trim(u8, part, "\n\r"));
                     }
 
-                    if (std.mem.eql(u8, msg[0..4], "QUIT")) {
+                    if (msg.len >= 4 and std.mem.eql(u8, msg[0..4], "QUIT")) {
                         conn.stream.close();
                         _ = clients.orderedRemove(pollfd.fd);
                         break;
+                    } else if (msg.len >= 4 and std.mem.eql(u8, msg[0..4], "STOP")) {
+                        break :outer;
                     }
 
                     if (parts.items.len < 2) {
-                        _ = try conn.stream.write("too short\n");
+                        _ = try conn.stream.write("early, too short\n");
                         break;
                     }
                     var command = parts.items[0];
                     var key = parts.items[1];
-                    std.debug.print("command: '{s}'\n", .{command});
-                    std.debug.print("key: '{s}'\n", .{key});
+                    // std.debug.print("command: '{s}'\n", .{command});
+                    // std.debug.print("key: '{s}'\n", .{key});
 
                     if (std.mem.eql(u8, command, "GET")) {
                         std.debug.print("message is GET\n", .{});
-                        var list = ArrayList(u8).init(allocator);
-                        defer list.deinit();
-                        var iter = db.iterator();
-                        while (iter.next()) |pair| {
+                        // var list = ArrayList(u8).init(allocator);
+                        // defer list.deinit(); // INTENTIONAL LEAK
+                        var db_iter = db.iterator();
+                        while (db_iter.next()) |pair| {
+                            std.debug.print("key: {}, value: '{}'\n", .{ pair.key_ptr, pair.value_ptr });
                             std.debug.print("key: {s}, value: '{s}'\n", .{ pair.key_ptr.*, pair.value_ptr.* });
+                            std.debug.print("key: {d}, value: '{d}'\n", .{ pair.key_ptr.*, pair.value_ptr.* });
                         }
                         var response = db.get(key); // orelse "";
                         if (response) |val| {
                             std.debug.print("response: {s}\n", .{val});
-                            _ = try std.fmt.format(list.writer(), "{s}\n", .{val});
+
+                            var response_string = try std.fmt.allocPrint(allocator, "{s}\n", .{val});
+                            defer allocator.free(response_string);
+
+                            // _ = try std.fmt.format(list.writer(), "{s}\n", .{val});
                             // _ = try std.fmt.format(list.writer(), "\n", .{});
-                            _ = try conn.stream.write(list.items);
+                            _ = try conn.stream.write(response_string);
                         } else {
                             _ = try conn.stream.write("\n");
                         }
                     } else if (std.mem.eql(u8, command, "SET")) {
                         if (parts.items.len < 3) {
-                            _ = try conn.stream.write("too short\n");
+                            _ = try conn.stream.write("SET, too short\n");
                             break;
                         }
                         var value = parts.items[2];
@@ -120,10 +137,21 @@ pub fn main() anyerror!void {
                         // var list = ArrayList(u8).init(allocator);
                         // defer list.deinit();
 
-                        var key2 = try allocator.dupe(u8, key);
-                        var value2 = try allocator.dupe(u8, value);
-                        _ = try db.put(key2, value2);
-                        std.debug.print("keys: {s}\n", .{db.keys()});
+                        // var key2 = try allocator.dupe(u8, key);
+                        // defer allocator.free(key2);
+                        // var value2 = try allocator.dupe(u8, value);
+                        // defer allocator.free(value2);
+                        // std.debug.print("key2: '{s}'\n", .{key2});
+                        // std.debug.print("key2: '{d}'\n", .{key2});
+                        // std.debug.print("typeInfo(key2): '{}'\n", .{@typeInfo(@TypeOf(key2))});
+                        _ = try db.put(key, value);
+
+                        var keys_iter = db.iterator();
+                        std.debug.print("keys: ", .{});
+                        while (keys_iter.next()) |pair| {
+                            std.debug.print("{s}, ", .{pair.key_ptr.*});
+                        }
+                        std.debug.print("\n", .{});
                         var iter = db.iterator();
                         while (iter.next()) |pair| {
                             std.debug.print("key: {s}, value: '{s}'\n", .{ pair.key_ptr, pair.value_ptr });
@@ -134,24 +162,40 @@ pub fn main() anyerror!void {
                         _ = try conn.stream.write("OK\n");
                     } else if (std.mem.eql(u8, command, "DEL")) {
                         // var removed = db.remove(key);
-                        var removed = db.swapRemove(key);
-                        if (removed) {
+                        // var removed = db.swapRemove(key);
+                        var entryOpt = db.get(key);
+                        if (entryOpt) |entry| {
+                            std.debug.print("key: {s}'\n", .{entry});
+
+                            // Seems like don't need to free for an array hash map?
+
+                            // try expect(@TypeOf(entry.key_ptr.*) == []const u8);
+                            // try expect(@TypeOf(entry.key_ptr) == *[]const u8);
+                            // allocator.free(entry.key_ptr.*);
+                            // allocator.free(entry.value_ptr.*);
+                            // allocator.destroy(entry.key_ptr);
+                            // _ = db.swapRemove(key);
+                            // _ = db.orderedRemove(key);
+                            _ = db.remove(key);
+
                             _ = try conn.stream.write("1\n");
                         } else {
                             _ = try conn.stream.write("0\n");
                         }
                     } else if (std.mem.eql(u8, command, "INCR")) {
                         std.debug.print("message is INCR\n", .{});
-                        var key2 = try allocator.dupe(u8, key);
-                        if (db.contains(key)) {
-                            var response = db.get(key).?;
-                            if (std.fmt.parseInt(i32, response, 10)) |number| {
+                        var existing_string_opt = db.get(key);
+                        if (existing_string_opt) |existing_string| {
+                            if (std.fmt.parseInt(i32, existing_string, 10)) |number| {
                                 var new_value = number + 1;
                                 var list = ArrayList(u8).init(allocator);
+                                defer list.deinit();
 
                                 _ = try std.fmt.format(list.writer(), "{?}", .{new_value});
-                                std.debug.print("putting {s}\n", .{list.items});
-                                _ = try db.put(key2, list.items);
+                                // std.debug.print("putting {s}\n", .{list.items});
+                                var new_str = try allocator.dupe(u8, list.items);
+                                defer allocator.free(new_str);
+                                _ = try db.put(key, new_str);
 
                                 var list2 = ArrayList(u8).init(allocator);
                                 defer list2.deinit();
@@ -161,21 +205,25 @@ pub fn main() anyerror!void {
                                 _ = try conn.stream.write("ERR value is not an integer or out of range\n");
                             }
                         } else {
-                            _ = try db.put(key2, "1");
+                            _ = try db.put(key, "1");
                             _ = try conn.stream.write("1\n");
                         }
+                    } else if (std.mem.eql(u8, command, "STOP")) {
+                        break :outer;
                     } else {
                         std.debug.print("unknown command\n", .{});
                         _ = try conn.stream.write("server: Unknown command\n");
                     }
-
-                    parts.deinit();
                 }
             }
         }
     }
 
-    std.os.exit(0);
+    // Memory leak playground
+    const bytes = try allocator.alloc(u8, 100);
+    defer allocator.free(bytes);
+    const u32_ptr = try allocator.create(u32);
+    defer allocator.destroy(u32_ptr);
 }
 
 // test "simple test" {
